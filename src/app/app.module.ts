465ebd9948b0b4189da15e2ba392a9e4ebf03895
AnuchaDev <anuchadev@gmail.com>
2021-03-05 18:30:39 +0700

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,34 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDExizW9I2H0sA49CNTaPNht5cwOZiFwRE",
+  authDomain: "todolist-61bc9.firebaseapp.com",
+  projectId: "todolist-61bc9",
+  storageBucket: "todolist-61bc9.appspot.com",
+  messagingSenderId: "692122633480",
+  appId: "1:692122633480:web:0c41d728f311aa3195d8bd",
+  measurementId: "G-R2XEWHVY3Z"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+     AngularFireDatabaseModule,
+     AngularFireAuthModule,
+     AngularFireStorageModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
+import { AngularFireDatabase } from '@angular/fire/database';
 import { Component } from '@angular/core';
+import { Subscriber } from 'rxjs';
+import { AlertController, NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,87 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate: string;
+  myTask: String;
+  addTask: boolean;
+  tasks = [];
 
-  constructor() {}
+  constructor(
+    public afDB: AngularFireDatabase,
+    private alertCtrl: AlertController,
+    public navCtrl: NavController,
+    private router: Router,
+  ) {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('en-US', options);
+    this.getTasks();
+  }
 
+  addTaskToFirebase() {
+    console.log(new Date());
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false,
+    });
+    this.showForm();
+  }
+
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+  getTasks() {
+    this.afDB
+      .list('Tasks/')
+      .snapshotChanges(['child_added', 'child_removed'])
+      .subscribe((actions) => {
+        this.tasks = [];
+        actions.forEach((action) => {
+          console.log('Tasks: ' + action.payload.exportVal().text);
+          this.tasks.push({
+            key: action.key,
+            text: action.payload.exportVal().text,
+            hour: action.payload.exportVal().date.substring(5, 10),
+            checked: action.payload.exportVal().checked,
+          });
+        });
+      });
+  }
+  changeCheckState(ev: any) {
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
+  }
+
+  async updateTask(key) {
+    console.log(key);
+    let alert = this.alertCtrl.create({
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'New task',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Edit',
+          handler: (data) => {
+            console.log(data.task);
+            this.afDB.object('Tasks/' + key).update({ text: data.task });
+            location.reload(true);
+          },
+        },
+      ],
+    });
+    (await alert).present();
+  }
 }
